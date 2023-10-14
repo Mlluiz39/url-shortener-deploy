ff22352215acdda70dc11e7c3527dc96504c9273
Marcelo Luiz <mlluizpereira39@gmail.com>
2023-10-14 16:24:19 -0300

    add new endpoint

--- src/config/Constants.ts
+++ src/config/Constants.ts
@@ -2,6 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const config = {
-  API_URL: process.env.API_URL || 'http://localhost:3000',
+  API_URL: process.env.API_URL,
   MONGO_CONNECTION: process.env.MONGO_CONNECTION,
 }
